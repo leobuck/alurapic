a4483eccc3c2ad95f8843e94c51dcc102dff5d2f
leobuck <leobuck12@gmail.com>
2022-03-16 21:53:15 -0300

    $http e promises

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,9 @@ export default {
     }
   },
   created() {
-    alert('Criei o componente');
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 </script>
