58b9f3b0ffaafd9536335a3d4c98cdb9c04b5d3b
leobuck <leobuck12@gmail.com>
2023-03-26 20:27:58 -0300

    Estilos e escopo

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -12,7 +12,7 @@
   }
 </script>
   
-<style>
+<style scoped>
   .painel {
     padding: 0 auto;
     border: solid 2px grey;
@@ -32,6 +32,10 @@
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+  }
+
+  * {
+    box-shadow: 5px 5px 10px black;
   }
 </style>
   
