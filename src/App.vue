ec6a3196bd91af2be43c03a15ac30848a3261af8
leobuck <leobuck12@gmail.com>
2024-02-12 15:01:37 -0300

    Filtrando efetivamente a lista

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1 class="centralizado">{{ titulo }}</h1>
     <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre por parte do título" />
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
         </meu-painel>
@@ -24,6 +24,16 @@ export default {
       titulo: "Alurapic",
       fotos: [],
       filtro: ""
+    }
+  },
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
     }
   },
   created() {
