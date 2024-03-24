fd7eb87a22b7cb9f3103821a7e447eb5b27b2473
leobuck <leobuck12@gmail.com>
2024-03-24 19:59:00 -0300

    Deixando tudo mais alegre com efeitos

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -3,9 +3,11 @@
     <h2 class="painel-titulo" @dblclick="visivel = !visivel">
       {{ titulo }}
     </h2>
-    <div class="painel-conteudo" v-show="visivel">
-      <slot></slot>
-    </div>
+    <transition name="painel-fade">
+      <div class="painel-conteudo" v-show="visivel">
+        <slot></slot>
+      </div>
+    </transition>
   </div>
 </template>
   
@@ -45,6 +47,14 @@
 
   * {
     box-shadow: 5px 5px 10px black;
+  }
+
+  .painel-fade-enter, .painel-fade-leave-active {
+    opacity: 0;
+  }
+
+  .painel-fade-enter-active, .painel-fade-leave-active {
+    transition: opacity .4s;
   }
 </style>
   
