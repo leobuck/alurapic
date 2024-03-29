70ff2654dd0452a1e0693356ce4d1467c8cfe291
leobuck <leobuck12@gmail.com>
2024-03-29 11:07:00 -0300

    Transformando o menu em componente

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,16 +1,16 @@
 <template>
-    <div>
-        <h1 class="centralizado">{{ titulo }}</h1>
-        <input type="search" class="filtro" @input="filtro = $event.target.value"
-            placeholder="Filtre por parte do título" />
-        <ul class="lista-fotos">
-            <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-                <meu-painel :titulo="foto.titulo">
-                    <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
-                </meu-painel>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <input type="search" class="filtro" @input="filtro = $event.target.value"
+      placeholder="Filtre por parte do título" />
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -18,58 +18,58 @@ import Painel from "../shared/painel/Painel.vue";
 import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
 
 export default {
-    components: {
-        "meu-painel": Painel,
-        "imagem-responsiva": ImagemResponsiva
-    },
-    data() {
-        return {
-            titulo: "Alurapic",
-            fotos: [],
-            filtro: ""
-        };
-    },
-    computed: {
-        fotosComFiltro() {
-            if (this.filtro) {
-                let exp = new RegExp(this.filtro.trim(), "i");
-                return this.fotos.filter(foto => exp.test(foto.titulo));
-            } else {
-                return this.fotos;
-            }
-        }
-    },
-    created() {
-        this.$http
-            .get("http://localhost:3000/v1/fotos")
-            .then(res => res.json())
-            .then(
-                fotos => (this.fotos = fotos),
-                err => console.log(err)
-            );
+  components: {
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva
+  },
+  data() {
+    return {
+      titulo: "Alurapic",
+      fotos: [],
+      filtro: ""
+    };
+  },
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
     }
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(
+        fotos => (this.fotos = fotos),
+        err => console.log(err)
+      );
+  }
 };
 </script>
 
 <style>
 .centralizado {
-    text-align: center;
+  text-align: center;
 }
 
 .lista-fotos {
-    list-style: none;
+  list-style: none;
 }
 
 .lista-fotos .lista-fotos-item {
-    display: inline-block;
+  display: inline-block;
 }
 
 .imagem-responsiva {
-    width: 100%;
+  width: 100%;
 }
 
 .filtro {
-    display: block;
-    width: 100%;
+  display: block;
+  width: 100%;
 }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import Cadastro from "./components/cadastro/Cadastro.vue";
 import Home from "./components/home/Home.vue";
 
 export const routes = [
-  { path: "", component: Home, titulo: 'Página Inicial' },
-  { path: "/cadastro", component: Cadastro, titulo: 'Cadastro' }
+  { path: "", component: Home, titulo: "Página Inicial" },
+  { path: "/cadastro", component: Cadastro, titulo: "Cadastro" }
 ];
