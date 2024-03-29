fa10e4fc7ec8e62f6ce6a268cac81b91f612265e
leobuck <leobuck12@gmail.com>
2024-03-29 10:19:08 -0300

    O módulo VueRouter: instalação e registro

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-import VueResource from 'vue-resource';
+import VueResource from "vue-resource";
+import VueRouter from "vue-router";
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
